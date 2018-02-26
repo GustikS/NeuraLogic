fa4427ea52b6e2de419606526d894f7d0ae3720a
gustas <souregus@gmail.com>
2018-02-26 18:48:09 +0100

    grammars cleanup

--- Parsing/src/main/antlr/parsers/hello/hello_worldBaseListener.java
+++ Parsing/src/main/antlr/parsers/hello/hello_worldBaseListener.java
@@ -1,43 +1,28 @@
-// Generated from /home/gusta/googledrive/Github/NeuraLogic/Parsing/src/main/antlr/Test.g4 by ANTLR 4.7
-package parsers.Test;
-
-//    package parsers.Test;
-
+// Generated from /home/gusta/googledrive/Github/NeuraLogic/Parsing/src/main/antlr/hello_world.g4 by ANTLR 4.7
+package parsers.hello;
 
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 /**
- * This class provides an empty implementation of {@link TestListener},
+ * This class provides an empty implementation of {@link hello_worldListener},
  * which can be extended to create a listener which only needs to handle a subset
  * of the available methods.
  */
-public class TestBaseListener implements TestListener {
+public class hello_worldBaseListener implements hello_worldListener {
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterMain(TestParser.MainContext ctx) { }
+	@Override public void enterRule2(hello_worldParser.Rule2Context ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitMain(TestParser.MainContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterName(TestParser.NameContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitName(TestParser.NameContext ctx) { }
+	@Override public void exitRule2(hello_worldParser.Rule2Context ctx) { }
 
 	/**
 	 * {@inheritDoc}
