d8c5df6bf88a7d651abb1988f8475a7c84d15b60
GustikS <souregus@gmail.com>
2022-10-24 19:42:21 +0200

    updating Value tests

--- Algebra/src/test/java/cz/cvut/fel/ida/algebra/values/ValueTest.java
+++ Algebra/src/test/java/cz/cvut/fel/ida/algebra/values/ValueTest.java
@@ -110,7 +110,7 @@ class ValueTest {
         VectorValue big = new VectorValue(Arrays.asList(1.0, 2.0, 3.0));
 
         int compareTo = big.compareTo(small);
-        assertEquals(0, compareTo);
+        assertEquals(1, compareTo);
     }
 
     @TestAnnotations.Fast
@@ -313,8 +313,8 @@ class ValueTest {
         Value b = new VectorValue(Arrays.asList(2.0,2.0,3.0));
         Value c = new VectorValue(Arrays.asList(2.0,2.0,6.0));
 
-        assertEquals(a.compareTo(b),1);
-        assertEquals(b.compareTo(c),-1);
-        assertEquals(a.compareTo(c),-1);
+        assertEquals(1,a.compareTo(b));
+        assertEquals(-1,b.compareTo(c));
+        assertEquals(-1,a.compareTo(c));
     }
 }
