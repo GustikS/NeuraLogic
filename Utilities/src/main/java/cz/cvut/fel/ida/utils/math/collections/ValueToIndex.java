529082b8d34e28bbf71c02993f63b6c87e7bd2bd
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2025-12-07 23:18:10 +0100

    Improve performance of ValueToIndex by avoiding multiple lookups and better memory management

--- Utilities/src/main/java/cz/cvut/fel/ida/utils/math/collections/ValueToIndex.java
+++ Utilities/src/main/java/cz/cvut/fel/ida/utils/math/collections/ValueToIndex.java
@@ -8,11 +8,6 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package cz.cvut.fel.ida.utils.math.collections;
 
 import java.util.*;
@@ -24,19 +19,24 @@ import java.util.*;
  * @author Ondra
  */
 public class ValueToIndex<T> {
-    
+
     private int lastIndex = 0;
-
     private int max = 0;
-    
-    private LinkedHashMap<T,Integer> valueToIndex = new LinkedHashMap<T,Integer>();
-    
-    private LinkedHashMap<Integer,T> indexToValue = new LinkedHashMap<Integer,T>();
 
-    public ValueToIndex(){}
+    private final HashMap<T, Integer> valueToIndex;
+    private final HashMap<Integer, T> indexToValue;
 
-    public ValueToIndex(int startIndex){
+    private static final int INITIAL_CAPACITY = 16;
+
+    public ValueToIndex() {
+        this(0);
+    }
+
+    public ValueToIndex(int startIndex) {
         this.lastIndex = startIndex;
+        this.max = startIndex - 1;
+        this.valueToIndex = new HashMap<>(INITIAL_CAPACITY);
+        this.indexToValue = new HashMap<>(INITIAL_CAPACITY);
     }
 
     /**
@@ -44,16 +44,19 @@ public class ValueToIndex<T> {
      * @param t the object
      * @return the unique integer representing the object
      */
-    public int valueToIndex(T t){
-        if (!valueToIndex.containsKey(t)){
-            valueToIndex.put(t, lastIndex);
-            indexToValue.put(lastIndex, t);
-            max = lastIndex;
-            lastIndex++;
+    public int valueToIndex(T t) {
+        Integer index = valueToIndex.get(t);
+        if (index != null) {
+            return index;
         }
-        return valueToIndex.get(t);
+
+        int newIndex = lastIndex++;
+        valueToIndex.put(t, newIndex);
+        indexToValue.put(newIndex, t);
+        max = newIndex;
+        return newIndex;
     }
-    
+
     /**
      * Converts the given unique identifier back to the original object (the method
      * valueToIndex with this object must have had been called prior to calling this method,
@@ -61,27 +64,25 @@ public class ValueToIndex<T> {
      * @param index the unique identifier of the object
      * @return the object corresponding to the given unique identifier or null if there is no such object
      */
-    public T indexToValue(int index){
-        if (!indexToValue.containsKey(index))
-            return null;
-        else
-            return indexToValue.get(index);
+    public T indexToValue(int index) {
+        return indexToValue.get(index);
     }
 
-    public T getValue(int index){
-        return this.indexToValue.get(index);
+    public T getValue(int index) {
+        return indexToValue.get(index);
     }
 
-    public int getIndex(T value){
-        return this.valueToIndex.get(value);
+    public int getIndex(T value) {
+        Integer index = valueToIndex.get(value);
+        return index != null ? index : -1;
     }
 
-    public boolean containsValue(T value){
-        return this.valueToIndex.containsKey(value);
+    public boolean containsValue(T value) {
+        return valueToIndex.containsKey(value);
     }
 
-    public boolean containsIndex(int index){
-        return this.indexToValue.containsKey(index);
+    public boolean containsIndex(int index) {
+        return indexToValue.containsKey(index);
     }
 
     /**
@@ -89,35 +90,43 @@ public class ValueToIndex<T> {
      * @param key the unique identifier
      * @param value the object
      */
-    public void put(int key, T value){
-        this.valueToIndex.put(value, key);
-        this.indexToValue.put(key, value);
-        this.lastIndex = Math.max(this.lastIndex, key+1);
-        max = Math.max(lastIndex, key);
+    public void put(int key, T value) {
+        valueToIndex.put(value, key);
+        indexToValue.put(key, value);
+
+        if (key >= lastIndex) {
+            lastIndex = key + 1;
+            max = key;
+        } else if (key > max) {
+            max = key;
+        }
     }
-    
+
     /**
      * Creates a set of unique identifiers for the objects iterable the given collection.
      * @param coll the collection of objects
      * @return the set of unique identifiers for the objects iterable the collection <em>coll</em>
      */
-    public Set<Integer> valuesToIndices(Collection<T> coll){
-        Set<Integer> retVal = new HashSet<Integer>();
-        for (T t : coll){
+    public Set<Integer> valuesToIndices(Collection<T> coll) {
+        Set<Integer> retVal = new HashSet<>(coll.size());
+        for (T t : coll) {
             retVal.add(valueToIndex(t));
         }
         return retVal;
     }
-    
+
     /**
      * Creates a set of objects for the unique identifiers iterable the given collection.
      * @param coll the collection of unique identifiers
      * @return the set of objects for the unique identifiers iterable the collection <em>coll</em>
      */
-    public Set<T> indicesToValues(Collection<Integer> coll){
-        Set<T> retVal = new HashSet<T>();
-        for (Integer i : coll){
-            retVal.add(indexToValue(i));
+    public Set<T> indicesToValues(Collection<Integer> coll) {
+        Set<T> retVal = new HashSet<>(coll.size());
+        for (Integer i : coll) {
+            T value = indexToValue.get(i);
+            if (value != null) {
+                retVal.add(value);
+            }
         }
         return retVal;
     }
@@ -129,37 +138,41 @@ public class ValueToIndex<T> {
     public Map<Integer,T> indicesToValuesMap(){
         return this.indexToValue;
     }
-    
+
     /**
      * 
      * @return number of elements for which there are the unique IDs
      */
-    public int size(){
-        return this.valueToIndex.size();
+    public int size() {
+        return valueToIndex.size();
     }
-    
+
     /**
      * 
      * @return the objects for which there are the unique IDs
      */
-    public Set<T> values(){
+    public Set<T> values() {
         return valueToIndex.keySet();
     }
-    
+
     /**
      * 
      * @return the unique IDs
      */
-    public Set<Integer> indices(){
+    public Set<Integer> indices() {
         return indexToValue.keySet();
     }
-    
-    @Override
-    public String toString(){
-        return this.valueToIndex.toString();
+
+    /**
+     * Returns the maximum index assigned.
+     * @return the max index
+     */
+    public int max() {
+        return max;
     }
 
-    public int max(){
-        return this.max;
+    @Override
+    public String toString() {
+        return valueToIndex.toString();
     }
 }
